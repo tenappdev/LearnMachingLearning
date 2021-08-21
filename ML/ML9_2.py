fb735e59aa9c7af2da6859f6eefe8ae45548d676
tenappdev <pirun_6@hotmail.com>
2021-08-21 21:09:42 +0700

    Create ML9_2.py
    ไฟล์ ML9 ที่ใช้ scatter วาดกราฟแทน

---
+++ ML/ML9_2.py
@@ -0,0 +1,27 @@
+'''
+study from https://youtu.be/bALu2G_iu1c
+from equation y = ax +b
+x ตัวแปรอิสระ
+y ตัวแปรตาม
+a ความชันของเส้นตรง
+b ระยะตัดแกน y
+
+We must install "pip install numpy" first
+'''
+import numpy as np
+import matplotlib.pyplot as plt
+
+#สร้างตัวเลข -5 ถึง 5 จำนวน 100 ชุด
+x = np.linspace(-5,5,100) #ตัวแปรอิสระ
+print(x) #เมื่อ print ออกมาจะได้ x มาเป็นขโยง
+
+#y = ax + b
+y = 2*x + 1 #เมื่อเอา x ที่เป้นขโยงมาเข้าสมการนี้จะได้ y เป็นขโยงที่จำนวนสมาชิกเท่ากับ x
+
+plt.scatter(x,y) #ออกเป็นจุด
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend(loc='upper left')
+plt.title("Graph y=2x+1")
+plt.grid()
+plt.show()
